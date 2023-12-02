198cc2079a771bc6b2bebe8f84315159bcb1e7e3
Mitchell Barry <mitch.barry@gmail.com>
2023-12-02 14:31:34 -0600

    Added Blazorise to Counter

--- src/OpenWish.Client/Program.cs
+++ src/OpenWish.Client/Program.cs
@@ -1,11 +1,22 @@
 using OpenWish.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();

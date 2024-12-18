cb957c0202e8d44a64bfa329ccbfbd2cd7854181
Mitchell Barry <mitch.barry@gmail.com>
2024-12-17 22:01:31 -0600

    start adding sql to aspire host

--- src/OpenWish.AppHost/Program.cs
+++ src/OpenWish.AppHost/Program.cs
@@ -1,6 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.OpenWish_ApiService>("apiservice");
+var sql = builder.AddSqlServer("sql")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var db = sql.AddDatabase("database");
+
+var apiService = builder.AddProject<Projects.OpenWish_ApiService>("apiservice")
+    .WithReference(db)
+    .WaitFor(db);
 
 builder.AddProject<Projects.OpenWish_Web>("webfrontend")
     .WithExternalHttpEndpoints()

0717f73491817c5719de5bc8d278636094dff6d8
Mitchell Barry <mitch.barry@gmail.com>
2024-12-30 19:31:08 -0600

    Troubleshooting binding

--- src/OpenWish.Web/Program.cs
+++ src/OpenWish.Web/Program.cs
@@ -41,9 +41,10 @@ var connectionString = builder.Configuration.GetConnectionString("OpenWish")
     ?? throw new InvalidOperationException("Connection string 'OpenWish' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString)
+    options.UseNpgsql(connectionString + ";Include Error Detail=true;")
         // HMM... https://github.com/dotnet/efcore/issues/34431
         .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning))
+        .EnableSensitiveDataLogging()
     );
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();

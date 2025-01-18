8bebba89777fb7369c146e77fa380d349b73f8d5
Mitchell <mitch.barry@gmail.com>
2025-01-18 14:24:11 -0600

    Improve style consistency of Event and Wishlist cards

--- src/OpenWish.Shared/Models/WishlistModel.cs
+++ src/OpenWish.Shared/Models/WishlistModel.cs
@@ -2,12 +2,10 @@
 
 public class WishlistModel : BaseEntityModel
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string OwnerId { get; set; }
+    public required string Name { get; set; }
+    public string? OwnerId { get; set; }
     public ApplicationUserModel? Owner { get; set; }
     public bool IsCollaborative { get; set; }
     public int ItemCount { get; set; }
-    public DateTimeOffset CreatedOn { get; set; }
     public ICollection<WishlistItemModel> Items { get; set; } = [];
 }
